f5435e20f67a083b6897aadc9342a0cd1428d5f2
Iswat <ibeetek@gmail.com>
2022-12-01 16:54:26 +0000

    Pass exercise 2 and 3 testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let word1 = word.split("");
+  word1 = word1.reverse();
+  let word2 = word1.join("");
+  return word2;
 
 };
 
