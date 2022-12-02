e2d0f09f57d9fd815e79bdcd8069bc03150fe199
Iswat <ibeetek@gmail.com>
2022-12-02 16:43:12 +0000

    Move result variable inside the function in 02-repeatstring exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let result = "";
+    if (num >= 0) {
+        for(let i=1; i<=num; i++) {
+            result += string;
+             
+        }
+        return result;
+    } else {
+        result = "ERROR";
+        
+    } 
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -1,15 +1,15 @@
 const reverseString = require('./reverseString')
 
 describe('reverseString', () => {
-  test.skip('reverses single word', () => {
+  test('reverses single word', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
 
-  test.skip('reverses multiple words', () => {
+  test('reverses multiple words', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh')
   })
 
-  test.skip('works with numbers and punctuation', () => {
+  test('works with numbers and punctuation', () => {
     expect(reverseString('123! abc!')).toEqual('!cba !321')
   })
   test('works with blank strings', () => {
