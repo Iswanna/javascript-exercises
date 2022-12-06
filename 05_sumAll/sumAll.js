7d195b0419e852bfbe0328be9140cfc668b58543
Iswat <ibeetek@gmail.com>
2022-12-06 20:24:49 +0000

    Complete 05_sumall testing 100%

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
-
-};
+const sumAll = function(firstNum, secondNum) {
+    let cumSum = 0;
+    let result;
+   let figOne = firstNum;
+   let figTwo = secondNum;
+   
+   if (firstNum === secondNum) {
+     figOne = firstNum;
+     figTwo = secondNum;
+   } else if (firstNum < secondNum) {
+     figOne = firstNum;
+     figTwo = secondNum;
+   } else if (firstNum > secondNum) {
+     figOne = secondNum;
+     figTwo = firstNum;
+   } 
+  
+   for (let i = figOne; i <= figTwo; i++) {
+     cumSum += i ;
+   
+ 
+   }
+ 
+ 
+   if (typeof figOne !== "number" || typeof figTwo !== "number") {
+     cumSum = "ERROR";
+   } else if (firstNum < 1 || secondNum < 1) {
+     cumSum = "ERROR";
+   } 
+   
+   return cumSum;
+ };
 
 // Do not edit below this line
 module.exports = sumAll;
