6c58aa40c55b318fec5df0d110203bfc548939c0
Iswat <ibeetek@gmail.com>
2022-12-06 11:18:16 +0000

    Complete 04_removefromarray testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
-    
+    let arg1 = arguments;
+  
+ 
+  let arr = arguments[0];
+  let resultg = "";
+  
+  
+  for (let i = 1; i < arg1.length; i++){
+    for (let j = 0; j < arr.length; j++) {
+      if (arg1[i] === arr[j]) {
+        resultg = arr.splice(j, 1);
+        j--
+      }
+  }
+    }
+  
+  return(arr);
 
 };
 
