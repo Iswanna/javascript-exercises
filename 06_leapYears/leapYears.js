39572de1c61b7eb616db2d6af083f78405262e17
Iswat <ibeetek@gmail.com>
2022-12-08 15:48:42 +0000

    Complete 06_leapyears testing exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(leapYr) {
+    let result;
+    if (!(leapYr % 4) && (leapYr % 100) || !(leapYr % 400)) {
+      result = true;
+    } else {
+      result = false;
+    }
+    return result;
+  };
+  
 
 // Do not edit below this line
 module.exports = leapYears;
