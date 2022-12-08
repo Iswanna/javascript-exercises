49ac5fdcfc36480af53fe600a195de6db141ddc7
Iswat <ibeetek@gmail.com>
2022-12-08 19:42:05 +0000

    Complete 07_tempconversion testing execise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fah) {
+  let fahResult = (fah - 32) * (5 / 9);
+  fahResult = Math.round(fahResult * 10)/10 
 
+  return fahResult;
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  let degResult = (deg * (9/5)) + 32;
+  degResult = Math.round(degResult * 10)/10;
+  return degResult;
 };
 
 // Do not edit below this line
