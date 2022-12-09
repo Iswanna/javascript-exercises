7b2a59468726e2f07f4d2661ead63f59814132ba
Iswat <ibeetek@gmail.com>
2022-12-09 17:54:40 +0000

    Complete 09_palindromes testing exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (stringt) {
+    stringt = stringt.toLowerCase();
+    let stringt11 = stringt.match(/[a-z]/g);
+    
+    let stringt1 = stringt11.join("");
+    
+    
+    let stringtArra = stringt11.reverse();
+    
+    stringtArra = stringtArra.join("");
+    
+    if (stringt1 === stringtArra) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
