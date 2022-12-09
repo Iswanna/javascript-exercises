6374f04b9ed54a557f55ce62ab30cd4d90a2723e
Iswat <ibeetek@gmail.com>
2022-12-09 14:42:24 +0000

    Complete calculator testing exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
 const add = function() {
-	
+  let addUp = arguments[0];
+	for (let i=1; i<arguments.length; i++) {
+    addUp += arguments[i];
+  }
+  return addUp;
 };
 
 const subtract = function() {
-	
+	let subtractUp = arguments[0];
+	for (let i=1; i<arguments.length; i++) {
+    subtractUp -= arguments[i];
+  }
+  return subtractUp;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumUp = 0;
+	for (let i=0; i<arr.length; i++) {
+    sumUp += arr[i];
+  }
+  return sumUp;
 };
 
-const multiply = function() {
-
+const multiply = function(arrt) {
+  let multiplyUp = 1;
+	for (let i=0; i<arrt.length; i++) {
+    multiplyUp *= arrt[i];
+  }
+  return multiplyUp;
 };
 
 const power = function() {
-	
+	let powerUp = arguments[0];
+	for (let i=1; i<arguments.length; i++) {
+    powerUp **= arguments[i];
+  }
+  return powerUp;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let factorialUp = 1;
+	for (let i=fact; i > 0; i--) {
+    factorialUp *= i;
+  }
+  return factorialUp;
 };
+
 
 // Do not edit below this line
 module.exports = {
