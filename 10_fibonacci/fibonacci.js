028ee7df5d7c0f2cfda8109d823eb7039535fcc6
Iswat <ibeetek@gmail.com>
2022-12-10 16:50:55 +0000

    Complete 10_fibonacci testing exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    num = +num;
+    let nextNum;
+    if (num === 1) {
+      nextNum = 1;
+    } else if (num > 1) {
+        let n1 = 0;
+        let n2 = 1;
+        
+        for (let i=2; i<=num; i++) {
+          nextNum = n1 + n2;
+          n1 = n2;
+          n2 = nextNum;
+        }
+      } else {
+        nextNum = "OOPS"
+      }
+      
+    return nextNum;
+  };
 // Do not edit below this line
 module.exports = fibonacci;
