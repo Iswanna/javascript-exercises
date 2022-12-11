1ebacaa95f2b75253b2e98a2494ce68b23d18dc0
Iswat <ibeetek@gmail.com>
2022-12-11 11:35:28 +0000

    Complete 11_getthetitles testing exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(arr) {
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (i < arr.length-1) {
+        result += arr[i].title + " ";
+      } else if (i === arr.length-1) {
+        result += arr[i].title;
+      }
+       
+    }
+    result = result.split(" ");
+    return result;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
